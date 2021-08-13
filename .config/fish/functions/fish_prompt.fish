7379e58fc8aae1acc7cf5eb9992c59ff3514c61d
taekwombo <88032359+taekwombo@users.noreply.github.com>
2021-08-13 22:12:19 +0200

    chore(fish): update fish_prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,4 +1,4 @@
 function fish_prompt --description 'Write out the prompt'
-    echo -n -s ' ' (string repeat -n $SHLVL '>') ' ' (set_color normal) (prompt_pwd) ' ' 
+    echo -n -s (string repeat -n $SHLVL '>') ' ' (set_color normal) (prompt_pwd) ' ' 
 end
 
