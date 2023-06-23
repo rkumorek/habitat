f132e22e4928484ebda345132d6866397b6b5771
taekwombo <88032359+taekwombo@users.noreply.github.com>
2023-06-23 08:33:57 +0200

    chore: loop over all ./home files

--- install.fish
+++ install.fish
@@ -8,25 +8,29 @@ end
 set home $XDG_CONFIG_HOME
 set dir (realpath (dirname (status -f)))
 
-function link_config_dir --argument-names name
-    set -l path "$home/$name"
-
+function check_path --argument-names path
     if test -e $path
         read -l -P "Path $path already exists, delete? [y/n]? " confirmation 
 
         if test $confirmation = 'y'
             rm -r $path
         else
-            return
+            return 1
         end
     end
+end
 
-    ln -vs "$dir/config/$name" $home
+function link_config_dir --argument-names name
+    set -l path "$home/$name"
+
+    check_path $path && ln -vs "$dir/config/$name" $home
 end
 
 for filename in (basename -a ./config/*)
     link_config_dir $filename
 end
 
-ln -s $dir/home/.tmux.conf ~/.tmux.conf
+for filename in (basename -a ./home/{.,}*)
+    check_path ~/$filename && ln -s $dir/home/$filename ~/$filename
+end
 
